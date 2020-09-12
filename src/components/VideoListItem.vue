e5802b7484c568d160d2827ebd72ec7f14077b23
Ahmad Anvari <ahmad.anv@gmail.com>
2020-09-12 10:06:37 +0430

    Add VideoListItem component

---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <li class="list-group-item" @click="onVideoSelect">
+    <div class="media">
+      <img class="mr-3" :src="thumbnailURL">
+
+      <div class="media-body">
+        <h5 class="mt-0">{{ title }}</h5>
+<!--        {{ description }}-->
+      </div>
+    </div>
+
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'VideoListItem',
+  props: ['video'],
+  computed: {
+    thumbnailURL() {
+      return this.video.snippet.thumbnails.default.url;
+    },
+    title() {
+      return this.video.snippet.title
+    },
+    description() {
+      return this.video.snippet.description
+    },
+  },
+  methods: {
+    onVideoSelect() {
+      this.$emit('videoSelect', this.video);
+    }
+  }
+}
+</script>
+
+<style scoped>
+li {
+  cursor: pointer;
+}
+
+li:hover {
+  background-color: #eee;
+}
+</style>
